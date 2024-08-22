6db989a46fb22802d00ef986fe625be07edafe50
meng <monkeystylle@gmail.com>
2024-08-22 11:50:37 +0800

    updated stories category name

--- components/SampleText/SampleText.stories.tsx
+++ components/SampleText/SampleText.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import SampleText from './SampleText';
 
 const meta: Meta<typeof SampleText> = {
-  title: 'Example/SampleText',
+  title: 'Example Component/SampleText',
   component: SampleText,
   tags: ['autodocs'],
   parameters: {
--- stories/Page.stories.ts
+++ stories/Page.stories.ts
@@ -4,7 +4,7 @@ import { within, userEvent, expect } from '@storybook/test';
 import { Page } from './Page';
 
 const meta = {
-  title: 'Example/Page',
+  title: 'Example Component/Page',
   component: Page,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
